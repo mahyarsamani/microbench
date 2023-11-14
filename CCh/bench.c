792e5a255d14efb8944a2d8fdd72b3c06574e550
Mahyar Samani <msamani@ucdavis.edu>
2023-11-14 01:15:11 +0000

    Updating ubench code with Prajwal's code.

--- CCh/bench.c
+++ CCh/bench.c
@@ -2,35 +2,28 @@
 #include "randArr.h"
 #include "common.h"
 
-#define ASIZE 0
-#define STEP 16384
-#define ITERS 8
+#define STEP   16384
+#define ITERS     64
 
-__attribute__((noinline)) int loop(int zero)
-{
-    int t = 0, i, iter;
-    for (iter = 0; iter < ITERS; ++iter)
-    {
-        for (i = 0; i < STEP + zero; i += 1)
-        {
-            if (randArr[i])
-            {
-                t += 3 + 3 * t;
-            }
-            else
-            {
-                t -= 1 - 5 * t;
-            }
-        }
+__attribute__ ((noinline))
+int loop(int zero) {
+  int t = 0,i,iter;
+  for(iter=0; iter < ITERS; ++iter) {
+    for(i=0; i < STEP + zero; i+=1) {
+      if(randArr[i])  {
+        t+=3+3*t;
+      } else {
+        t-=1-5*t;
+      }
     }
-    return t;
+  }
+  return t;
 }
 
-int main(int argc, char *argv[])
-{
-    argc &= 10000;
-    ROI_BEGIN();
-    int t = loop(argc);
-    ROI_END();
-    volatile int a = t;
+int main(int argc, char* argv[]) {
+   argc&=10000;
+   ROI_BEGIN(); 
+   int t=loop(argc); 
+   ROI_END();
+   volatile int a = t;
 }
--- MC/bench.c
+++ MC/bench.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "common.h"
 
-#define ASIZE 8192
-#define STEP 128
+#define ASIZE   32768
+#define STEP    128
+#define ITERS   16
 
-float arr[ASIZE];
-float arr1[ASIZE];
+float arr1[ASIZE]; //each of 128 KB 
 float arr2[ASIZE];
 float arr3[ASIZE];
 float arr4[ASIZE];
@@ -15,30 +15,31 @@ float arr7[ASIZE];
 float arr8[ASIZE];
 float arr9[ASIZE];
 
-__attribute__((noinline)) float loop3(int zero)
-{
-    float f = 0;
-    for (int i = 0; i < ASIZE; i += 1)
-    {
-        int ind = i & (STEP - 1);
-        f += arr1[ind];
-        f += arr2[ind];
-        f += arr3[ind];
-        f += arr4[ind];
-        f += arr5[ind];
-        f += arr6[ind];
-        f += arr7[ind];
-        f += arr8[ind];
-        f += arr9[ind];
+__attribute__ ((noinline)) 
+float loop3(int zero) {
+  float f = 0;
+  for ( int j= 0; j < ITERS; j++) {
+    for(int i = 0; i < ASIZE; i+=1) {
+      int ind=i&(STEP-1);
+      f+=arr1[ind];
+      f+=arr2[ind];
+      f+=arr3[ind];
+      f+=arr4[ind];
+      f+=arr5[ind];
+      f+=arr6[ind];
+      f+=arr7[ind];
+      f+=arr8[ind];
+      f+=arr9[ind];
+    //f=f*f*f*f*f*f*f*f*f*f*f;
     }
-    return f;
+  }
+  return f;
 }
 
-int main(int argc, char *argv[])
-{
-    argc &= 10000;
-    ROI_BEGIN();
-    float f = loop3(argc);
-    ROI_END();
-    volatile float a = f;
+int main(int argc, char* argv[]) {
+   argc&=10000;
+   ROI_BEGIN(); 
+   float f =loop3(argc);
+   ROI_END();
+   volatile float a = f;
 }
