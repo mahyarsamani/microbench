1b83c64dd797db767eb39311e7abc709c48faf42
Mahyar Samani <msamani@ucdavis.edu>
2024-01-18 00:41:00 +0000

    Updating the run script.s

--- run_ubench.sh
+++ run_ubench.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-benchmarks=(CCa CCe CCh CCh_st CCl CCm CF1 CRd CRf CRm CS1 CS3 DP1d DP1f DPcvt
-            DPT DPTd ED1 EF EI EM1 EM5 MC MCS MD M_Dyn MI MIM MIM2 MIP ML2
-            ML2_BW_ld ML2_BW_ldst ML2_BW_st ML2_st MM MM_st STL2 STL2b)
+benchmarks=(DPcvt CCm CF1 MM EM5 ML2 MCS ED1 MM_st CRd MIM2 CS3 ML2_st MD
+            SVE_SAXPY M_Dyn EI DP1f EM1 MI MC CRm STL2 STL2b CCa ML2_BW_st
+            DP1d MIM DPT EF STc CCl CS1 CCh SVE_SUM DPTd CRf ML2_BW_ldst
+            CCh_st MIP ML2_BW_ld CCe)
 
-for bm in $benchmarks; do
+for bm in ${benchmarks[@]}; do
     echo $bm
     export PAPI_EVENTS="PAPI_L1_DCM,PAPI_L1_DCA,PAPI_L2_LDM,PAPI_L2_DCA,PAPI_L2_TCA,PAPI_L1_ICA,PAPI_L1_ICM"
-    export PAPI_OUTPUT_DIRECTORY=/home/msamani/darchr/novoverse/microbench_native/$bm/memory_data
+    export PAPI_OUTPUT_DIRECTORY=${PWD}/../data/azacca/single-core/microbench/$bm/memory_data
     ./$bm/bench.papi
     export PAPI_EVENTS="PAPI_TLB_DM,PAPI_STL_ICY,PAPI_BR_MSP,PAPI_BR_PRC,PAPI_BR_INS,PAPI_TOT_CYC"
-    export PAPI_OUTPUT_DIRECTORY=/home/msamani/darchr/novoverse/microbench_native/$bm/pipeline_data
+    export PAPI_OUTPUT_DIRECTORY=${PWD}/../data/azacca/single-core/microbench/$bm/pipeline_data
     ./$bm/bench.papi
 done
 
